3131e36b73676855ab95bd12791c705e3cd4385d
Benny Frempong <your@email.com>
2026-07-14 04:55:34 -0400

    allow CORS for vercel

--- src/api/main.py
+++ src/api/main.py
@@ -39,7 +39,7 @@ app = FastAPI(
 # Configure CORS for React frontend (Week 4)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
